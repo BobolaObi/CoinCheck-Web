e90acffe1ab4c4510e9ebb4596b864fd1f05cb8a
BOBOLA <w0772193@myscc.ca>
2022-05-10 16:51:16 -0400

    Implemented header Section - Needs work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { PrintApiComponent } from './print-api/print-api.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrintApiComponent,
+    HeaderComponent,
   ],
 
   imports: [
